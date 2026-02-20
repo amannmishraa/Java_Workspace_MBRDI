def45cb3e1ef6e20c9f335b9f8b248828563f070
amannmishraa <amann.mishra24@gmail.com>
2026-02-20 09:49:34 +0530

    singleton pattern

---
+++ src/com/DesignPattern/Test.java
@@ -0,0 +1,9 @@
+package com.example.DesignPattern;
+
+public class Test {
+    public static void main(String[] args) {
+        //Sample x=new Sample();
+        Sample.getSample().show();
+    }
+
+}
